55b9c561153a8e6b78c28281dcf845d9af3fc954
aishwaryasonawane <101195799+aishwaryasonawane@users.noreply.github.com>
2023-03-23 20:29:24 +0530

    Add files via upload

---
+++ productread/src/main/java/com/product/ProductreadApplication.java
@@ -0,0 +1,13 @@
+package com.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // It is a Configuration class
+public class ProductreadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductreadApplication.class, args);
+	}
+
+}
